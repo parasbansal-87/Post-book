ad5dc929b1d2595e595535c64ffb3ebddf9afea7
parasbansal-87 <parasbansal33@gmail.com>
2019-01-22 19:19:13 +0530

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,21 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
+
   		#Log the user in and redirect to the user's show page
+      log_in user
+      
+      # Rails automatically converts this to the user's profile page
+      redirect_to user 
   	else
-  		flash[:danger] = 'Invalid email/password combination'
+  		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
   	end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
