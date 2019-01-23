cc985136e5e492f2ecc5d9bff488341cd37df42e
parasbansal-87 <parasbansal33@gmail.com>
2019-01-23 16:54:05 +0530

    Implement advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
+  	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
 
   		#Log the user in and redirect to the user's show page
-      log_in user
+      log_in @user
+
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       
       # Rails automatically converts this to the user's profile page
-      redirect_to user 
+      redirect_to @user 
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
   		render 'new'
@@ -19,7 +21,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    # if part is the fix for the first subtle bug
+    log_out if logged_in?
     redirect_to root_url
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
@@ -12,4 +13,22 @@ class ActiveSupport::TestCase
   def is_logged_in?
   	!session[:user_id].nil?
   end
+
+  def log_in_as(user)
+  	session[:user_id] = user.id
+  end
 end
+
+
+class ActionDispatch::IntegrationTest
+
+	# Log in as a particular user
+	
+	# These paramters have default values set.
+	def log_in_as(user, password: 'password', remember_me: '1')
+		post login_path, params: {session: { email: user.email,
+																				 password: password,
+																				 remember_me: remember_me } }
+	end
+end
+
